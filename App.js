b17de12b794872b24e9ebe9a4f966ab01dbb4c05
Joel Jerushan <joeljeru1@gmail.com>
2022-03-28 10:49:28 +0530

    layout testing

--- App.js
+++ App.js
@@ -5,19 +5,6 @@ import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 import { AppNavigator } from './Views/navigation.component';
 
-import { Button, Icon } from '@ui-kitten/components';
-
-const FacebookIcon = (props) => (
-  <Icon name='facebook' {...props} />
-);
-
-const HomeScreen = () => (
-  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text category='h1'>HOME</Text>
-    <Button accessoryLeft={FacebookIcon}>Login with Facebook</Button>
-  </Layout>
-);
-
 export default () => (
   <>
     <IconRegistry icons={EvaIconsPack} />
--- Views/Public/Home/home.component.js
+++ Views/Public/Home/home.component.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
-import { Button, Divider, Layout, TopNavigation } from '@ui-kitten/components';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { Text, Button, Divider, Layout, TopNavigation, Icon } from '@ui-kitten/components';
 
 export const HomeScreen = ({ navigation }) => {
 
@@ -12,9 +12,37 @@ export const HomeScreen = ({ navigation }) => {
     <SafeAreaView style={{ flex: 1 }}>
       <TopNavigation title='MyApp' alignment='center'/>
       <Divider/>
-      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Layout style={styles.container}>
         <Button onPress={navigateDetails}>OPEN DETAILS</Button>
+        
+        <Layout style={styles.layout} level='4'>
+            <Text>4</Text>
+        </Layout>
+        <Layout style={styles.layout} level='3'>
+            <Text>3</Text>
+        </Layout>
+        <Layout style={styles.layout} level='2'>
+            <Text>2</Text>
+        </Layout>
+
+        <Layout style={styles.layout} level='1'>
+            <Text>1</Text>
+        </Layout>
+
       </Layout>
     </SafeAreaView>
   );
 };
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'row',
+    },
+    layout: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+});
